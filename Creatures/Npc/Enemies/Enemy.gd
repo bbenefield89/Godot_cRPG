eb8eb787509d33717f9ed856eeff21261d9c3481
Brandon Benefield <bsquared18@gmail.com>
2021-09-19 19:32:24 -0400

    More refactoring

--- Creatures/Npc/Enemies/Enemy.gd
+++ Creatures/Npc/Enemies/Enemy.gd
@@ -1,34 +1,5 @@
 extends "res://Creatures/Creature/Creature.gd"
 
-func _on_HurtBox_area_entered(_area):
-	"""
-	Not too sure what to do with this function right now
-	"""
-	pass
-#	stats.set_health(stats.health - area.get_parent().get_node("Stats").damage)
-#	area.trigger_attack_cooldown()
-
-
-func _physics_process(delta):
-	move_to_point_clicked(delta)
-
-
-func move_to_point_clicked(delta: float) -> void:
-	var distance_to_walk = Stats.current_movement_speed * delta
-	
-	while distance_to_walk > 0 and path.size() > 0:
-		var distance_to_next_point = position.distance_to(path[0])
-		
-		if distance_to_walk <= distance_to_next_point:
-			# warning-ignore:return_value_discarded
-			move_and_slide(
-				position.direction_to(path[0]) * Stats.current_movement_speed)
-		else:
-			path.remove(0)
-		
-		distance_to_walk -= distance_to_next_point
-
-
 func _on_Stats_zero_health():
 	queue_free()
 
