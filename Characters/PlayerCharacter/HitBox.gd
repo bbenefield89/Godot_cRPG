280b35375ed6c21e67aa664f3c723ea657339cb0
Brandon Benefield <bsquared18@gmail.com>
2021-09-18 22:48:09 -0400

    Actor HitBox now flips bool of "monitoring" instead of "monitorable"

--- Characters/PlayerCharacter/HitBox.gd
+++ Characters/PlayerCharacter/HitBox.gd
@@ -2,16 +2,10 @@ extends Area2D
 
 onready var attack_cooldown_timer := $AttackCooldownTimer
 
-var attack_cooldown_duration := 1.0 setget set_attack_cooldown_duration
-
-func trigger_attack_cooldown() -> void:
-	set_deferred("monitorable", false)
+func trigger_attack_cooldown(attack_cooldown_duration: float) -> void:
+	set_deferred("monitoring", false)
 	attack_cooldown_timer.start(attack_cooldown_duration)
 
 
-func set_attack_cooldown_duration(value: float) -> void:
-	attack_cooldown_duration = value
-
-
 func _on_AttackCooldownTimer_timeout():
-	set_deferred("monitorable", true)
+	set_deferred("monitoring", true)
