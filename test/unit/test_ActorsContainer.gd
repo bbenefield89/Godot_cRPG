3fdf3be68b2feb073d6544eb8a32358d8a4b9f49
Brandon Benefield <bsquared18@gmail.com>
2021-09-27 18:27:46 -0400

    Can now select actors by clicking on them

--- test/unit/test_ActorsContainer.gd
+++ test/unit/test_ActorsContainer.gd
@@ -1,6 +1,7 @@
 extends "res://addons/gut/test.gd"
 
 var ActorsContainer = load("res://Characters/ActorsContainer.gd")
+var ActorScene := load("res://Creatures/Actor/Actor.tscn")
 
 func test_select_only_this_actor():
 	var actors_container = partial_double(ActorsContainer).new()
@@ -85,3 +86,49 @@ func test_deselect_all_actors():
 	assert_signal_emitted_with_parameters(actors_container,
 			"alter_actors_portraits", [[], { "background_color": "#0b2b5c" }])
 	assert_signal_emitted(actors_container, "conceal_actor_bar")
+
+
+func test_is_actor_allowed_to_move_true():
+	var actors_container = partial_double(ActorsContainer).new()
+	actors_container.is_selecting_actor = false
+	var Actor = KinematicBody2D.new()
+	actors_container.actors_selected = [Actor]
+	var result = actors_container.is_actor_allowed_to_move(Actor)
+	assert_true(result)
+
+
+func test_is_actor_allowed_to_move_false():
+	var actors_container = partial_double(ActorsContainer).new()
+	actors_container.is_selecting_actor = true
+	var Actor = KinematicBody2D.new()
+	var result = actors_container.is_actor_allowed_to_move(Actor)
+	assert_false(result)
+
+
+func test_add_actor_to_party():
+	var actors_container = partial_double(ActorsContainer).new()
+	var Actor = double(ActorScene).instance()
+	var ActorSelectBox = Actor.get_node("SelectBox")
+	actors_container.add_actor_to_party(Actor)
+	assert_connected(ActorSelectBox, actors_container, "lmb_up")
+	assert_connected(ActorSelectBox, actors_container, "lmb_up_shift")
+	assert_connected(ActorSelectBox, actors_container, "lmb_down")
+	assert_connected(ActorSelectBox, actors_container, "on_mouse_exit")
+
+
+func test_set_selected_actors_target():
+	var actors_container = partial_double(ActorsContainer).new()
+	var Actor = double(ActorScene).instance()
+	actors_container.actors_selected = [Actor]
+	var Enemy := KinematicBody2D.new()
+	actors_container.set_selected_actors_target(Enemy)
+	for actor in actors_container.actors_selected:
+		assert_called(actor, "set_current_target", [Enemy])
+
+
+func test_set_is_selecting_actor():
+	var actors_container = partial_double(ActorsContainer).new()
+	actors_container.set_is_selecting_actor(true)
+	assert_true(actors_container.is_selecting_actor)
+	actors_container.set_is_selecting_actor(false)
+	assert_false(actors_container.is_selecting_actor)
